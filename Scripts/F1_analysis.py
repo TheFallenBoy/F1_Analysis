90b4b6f7b328471db7b18a19593284fa6636e05d
JonzorG <Jonathan.johansson2005@gmail.com>
2026-02-19 13:45:45 +0100

    Added base files

---
+++ Scripts/F1_analysis.py
@@ -0,0 +1,5 @@
+import time
+
+print("Retrieving F1-Data...")
+time.sleep(5)
+print("No data found :(")
---
+++ Scripts/setup_db.py
@@ -0,0 +1,5 @@
+# 1 Create The database
+# 2 Create the tables
+# 3 insert .csv
+import mysql.connector
+from dotenv import load_dotenv
